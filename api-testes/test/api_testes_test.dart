2a1d1580577e56e098a5259316dab359f8fa2c35
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-08-24 09:42:12 -0300

    Nomeclaturas

--- api-testes/test/api_testes_test.dart
+++ api-testes/test/api_testes_test.dart
@@ -1,6 +1,9 @@
 import 'package:api_testes/api_testes.dart';
 import 'package:test/test.dart';
 
+// Switch de testes é tudo que está dentro da função main 
+// group -> é possível executar somente os testes dentro do grupo.
+// Apenas uma questão de organização
 void main() {
   test("Deve efetuar o calculo IMC", (){
     final result = calcIMC(1.78, 64);
@@ -9,11 +12,13 @@ void main() {
   });
 
 
-  test("Deve lançar uma excessão se altura for menor que 1", (){
+ group("Excessões de parametros | ", (){
+   test("Deve lançar uma excessão se altura for menor que 1", (){
     expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
   });
 
     test("Deve lançar uma excessão se o peso for menor que 1", (){
     expect(() => calcIMC(1.78, 0), throwsA(isA<Exception>()));
   });
+ });
 }
