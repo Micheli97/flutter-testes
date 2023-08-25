05f6e208169cb959466e2d64385d2b8406067727
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-08-25 09:17:34 -0300

    Exercício - Modelando a regra

--- leitor_lcov/lib/leitor_lcov.dart
+++ leitor_lcov/lib/leitor_lcov.dart
@@ -1,9 +1,56 @@
+import 'dart:io';
+
+import 'package:leitor_lcov/line_report.dart';
+
 void main(){
-  final result = covarage('./coverage/lvcov.info');
+  final result = covarage('./coverage/lcov.info');
   print(result);
 }
 
 
 String covarage(String lcovPath){
-  return "";
+  final file = File(lcovPath);
+  final content = file.readAsLinesSync();
+
+
+  final reports = <LineReport>[];
+
+  var sf = "";
+  var lf = 0;
+  var lh = 0;
+
+  for (var  text in content) {
+    if(text == "end_of_record"){
+      final report = LineReport(lineFound:lf, lineHit: lh, sourceile: sf);
+      reports.add(report);
+      continue;
+    }
+
+    final line = text.split(":");
+
+    if(line[0] == "SF"){
+      sf = line[1];
+    } else if(line[0] == "LF"){
+      lf = int.parse(line[1]);
+    }else if(line[0] == "LH"){
+      lf = int.parse(line[1]);
+    }
+
+    print(text);
+    
+  }
+
+  int totalLF = 0;
+  int totalLH = 0;
+
+  for (var report in reports) {
+    totalLF += report.lineFound;
+    totalLH += report.lineHit;
+    
+  }
+
+  final percent = (totalLH / totalLF) * 100;
+
+
+  return "${percent.round()}%";
 }
---
+++ leitor_lcov/lib/line_report.dart
@@ -0,0 +1,8 @@
+class LineReport{
+ final  String sourceile;
+ final int lineFound;
+ final  int lineHit;
+
+
+ LineReport({required this.lineFound, required this.lineHit, required this.sourceile});
+}
