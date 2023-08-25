cd41def524b0bcc9a550477ae717af6166c87524
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-08-25 09:36:08 -0300

    Testes assincronos

--- testes_assincronos/bin/testes_assincronos.dart
+++
@@ -1,5 +0,0 @@
-import 'package:testes_assincronos/testes_assincronos.dart' as testes_assincronos;
-
-void main(List<String> arguments) {
-  print('Hello world: ${testes_assincronos.calculate()}!');
-}
--- testes_assincronos/test/testes_assincronos_test.dart
+++ testes_assincronos/test/testes_assincronos_test.dart
@@ -1,8 +1,20 @@
-import 'package:testes_assincronos/testes_assincronos.dart';
+import 'package:testes_assincronos/future.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+
+  test("Deve completar a requisição trazendo uma lista de nomes", (){
+    final future = getFutureList();
+
+    // Para verificar se a função async foi completada
+    expect(future , completes);
+
+    // Testar o tipo de retorno da função
+    expect(future , completion(isA<List<String>>()));
+
+    // Testa os dados retornados pela funçao
+    expect(future , completion(equals(["masterclass", "flutterando"])));
+
   });
+ 
 }
