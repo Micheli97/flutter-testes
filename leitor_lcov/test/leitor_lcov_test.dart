e8abc5d87f031aca498f26d341a8268c4d3a9e01
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-08-25 09:26:23 -0300

    Refatorando o código

--- leitor_lcov/test/leitor_lcov_test.dart
+++ leitor_lcov/test/leitor_lcov_test.dart
@@ -1,9 +1,18 @@
 import 'package:leitor_lcov/leitor_lcov.dart';
+import 'package:leitor_lcov/line_report.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("Deve pegar porcentagem de cobertura", (){
+  test("Deve pegar porcentagem de cobertura", () {
     final result = covarage('./coverage/lcov.info');
-    expect(result,"100%");
+    expect(result, "0%");
+  });
+
+  test("Deve calcular a porcentagem em 50%", () {
+    final result = calculatePercent([
+      LineReport(lineFound: 18, lineHit: 9, sourceile: ""),
+      LineReport(lineFound: 10, lineHit: 5, sourceile: ""),
+    ]);
+    expect(result, 50);
   });
 }
