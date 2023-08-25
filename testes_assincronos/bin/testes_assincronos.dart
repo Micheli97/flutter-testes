faf636faff51e75ad2b296476bd9fc632f8c3514
Paula Micheli <56922971+Micheli97@users.noreply.github.com>
2023-08-25 09:27:19 -0300

    Testes assincronos

---
+++ testes_assincronos/bin/testes_assincronos.dart
@@ -0,0 +1,5 @@
+import 'package:testes_assincronos/testes_assincronos.dart' as testes_assincronos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${testes_assincronos.calculate()}!');
+}
---
+++ testes_assincronos/test/testes_assincronos_test.dart
@@ -0,0 +1,8 @@
+import 'package:testes_assincronos/testes_assincronos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
